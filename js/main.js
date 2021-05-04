86f09e00361674c8cce6b247f72b8924f3cdf186
AnkaJS <37883449+AnkaJS@users.noreply.github.com>
2021-05-04 17:24:57 +0300

    Добавили правило для адаптива

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const swiper = new Swiper('.channel_slider', {
+const channelSlider = new Swiper('.channel_slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 5,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -9,3 +27,65 @@ const swiper = new Swiper('.channel_slider', {
     prevEl: '.channel-button-prev',
   },
 });
+
+const recommendedChannelSlider = new Swiper('.channel_slider_rec-video', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-prev-rec-video',
+    prevEl: '.channel-button-next-rec-video',
+  },
+});
+
+const recommendedSlider = new Swiper('.channel_slider_rec', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-rec',
+    prevEl: '.channel-button-prev-rec',
+  },
+});
+const searchBtn = document.querySelector('.mobile_search');
+const mobileSearch = document.querySelector('.input_group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  channelSlider.destroy()
+  recommendedChannelSlider.destroy()
+  recommendedSlider.destroy()
+}
